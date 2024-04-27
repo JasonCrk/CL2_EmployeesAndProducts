1dca9a44efc2b41b7d442a534933f35665002661
JasonCrk <emerzonkolki@gmail.com>
2024-04-27 17:57:13 -0500

    feat(SessionTokenEntity.java): Change id attribute data type (long to Long)

--- src/main/java/com/cibertec/CL2/models/SessionTokenEntity.java
+++ src/main/java/com/cibertec/CL2/models/SessionTokenEntity.java
@@ -15,7 +15,7 @@ public class SessionTokenEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(unique = true, nullable = false)
     private String token;
