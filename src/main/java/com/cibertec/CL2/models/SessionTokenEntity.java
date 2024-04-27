0e7e94cfa8fdf8820474dbe8b30ae42cd431be78
JasonCrk <emerzonkolki@gmail.com>
2024-04-27 15:54:48 -0500

    feat: Add session token entity

---
+++ src/main/java/com/cibertec/CL2/models/SessionTokenEntity.java
@@ -0,0 +1,38 @@
+package com.cibertec.CL2.models;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "session_token")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionTokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN")
+    private boolean revoked;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN")
+    private boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private EmployeeEntity employee;
+
+    @PrePersist
+    public void setDefaultValues() {
+        this.revoked = false;
+        this.expired = false;
+    }
+}
