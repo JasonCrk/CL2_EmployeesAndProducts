9a66060c01707fa68ff574bea9efc23319f162b1
JasonCrk <emerzonkolki@gmail.com>
2024-04-27 15:15:07 -0500

    feat: Initial commit

---
+++ src/main/java/com/cibertec/CL2/Cl2EmerzonJavierKolkiMartinezApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.CL2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2EmerzonJavierKolkiMartinezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2EmerzonJavierKolkiMartinezApplication.class, args);
+	}
+
+}
