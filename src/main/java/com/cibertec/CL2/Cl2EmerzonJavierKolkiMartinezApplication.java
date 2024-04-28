9b142b4b5e13cf4d7d2aef2198b96f642de1f80d
JasonCrk <emerzonkolki@gmail.com>
2024-04-28 01:09:34 -0500

    feat: Add default employee from command line

--- src/main/java/com/cibertec/CL2/Cl2EmerzonJavierKolkiMartinezApplication.java
+++ src/main/java/com/cibertec/CL2/Cl2EmerzonJavierKolkiMartinezApplication.java
@@ -1,7 +1,13 @@
 package com.cibertec.CL2;
 
+import com.cibertec.CL2.models.EmployeeEntity;
+import com.cibertec.CL2.repositories.EmployeeRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Cl2EmerzonJavierKolkiMartinezApplication {
@@ -10,4 +16,19 @@ public class Cl2EmerzonJavierKolkiMartinezApplication {
 		SpringApplication.run(Cl2EmerzonJavierKolkiMartinezApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner defaultEmployee(
+			EmployeeRepository employeeRepository,
+			PasswordEncoder passwordEncoder
+	) {
+		return args -> {
+			employeeRepository.save(EmployeeEntity.builder()
+					.name("Nombre del empleado") // Este es el username
+					.username("Empleado 1")
+					.key(passwordEncoder.encode("123456")) // Esta es la clave
+					.lastName("Apellido paterno")
+					.motherLastName("Apellido materno")
+					.build());
+		};
+	}
 }
